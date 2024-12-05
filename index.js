373fac2a6bfea8e9564351a7aa38df0134293448
RehmanAli10 <vrehmanali@gmail.com>
2024-12-05 12:16:50 +0500

    imported module replaceTemplate

--- index.js
+++ index.js
@@ -3,33 +3,23 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const renderingTemplate = require("./modules/replaceTemplate");
+
 // READING FILES
 const tempOverview = fs.readFileSync(
-  "./templates/template-overview.html",
+  `${__dirname}/templates/template-overview.html`,
   "utf-8"
 );
-const tempCard = fs.readFileSync("./templates/template-card.html", "utf-8");
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 const tempProducts = fs.readFileSync(
-  "./templates/template-product.html",
+  `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-const devData = fs.readFileSync("./dev-data/data.json", "utf-8");
+const devData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productsData = JSON.parse(devData);
-
-function renderingTemplate(temp, product) {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-}
 
 // CREATING SERVER
 const server = http.createServer(function (req, res) {
